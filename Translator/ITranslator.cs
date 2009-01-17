10cceb2aea79947c6a6bbe9ca7be067a9178772e
Kevin Thompson <kthompson@pelco.com>
2009-01-16 16:37:11 -0800

    Initial Commit

---
+++ Translator/ITranslator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil.Cil;
+
+namespace Translator
+{
+    public interface ITranslator
+    {
+        byte[] GetAssemblyBytes(Code opcode);
+    }
+}
