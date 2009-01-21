243863f98ab28f65972bb5d27bec09867e08806b
Kevin Thompson <kthompson@pelco.com>
2009-01-21 15:14:41 -0800

    * Translator/MethodDependencies.cs: Implement

---
+++ Translator/MethodDependencies.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using Microsoft.Glee.Drawing;
+
+namespace Translator
+{
+    public class MethodDependencies : BaseReflectionVisitor
+    {
+        public List<MethodDefinition> Dependencies { get; private set; }
+        public List<AssemblyDefinition> Assemblies { get; private set; }
+        public Graph Graph { get; private set; }
+
+        public MethodDependencies()
+        {
+            this.Dependencies = new List<MethodDefinition>();
+            this.Assemblies = new List<AssemblyDefinition>();
+            this.Graph = new Graph("Method Tree");
+        }
+
+        private void LoadAssemblies(MethodDefinition method)
+        {
+            var type = method.DeclaringType;
+            var module = type.Module;
+            var asm = module.Assembly;
+            this.Assemblies.Add(asm);
+
+            var resolver = asm.Resolver;
+            foreach (AssemblyNameReference reference in module.AssemblyReferences)
+		        this.Assemblies.Add(resolver.Resolve(reference));
+        }
+
+        private MethodDefinition[] ResolveMemberReference(MemberReference member)
+        {
+            var typeName = member.DeclaringType.FullName;
+            foreach (var asm in this.Assemblies)
+            {
+                foreach (ModuleDefinition module in asm.Modules)
+                {
+                    var type = module.Types[typeName];
+                    if (type == null)
+                        continue;
+
+                    return type.Methods.GetMethod(member.Name);
+                }
+            }
+            return new MethodDefinition[]{};
+        }
+
+        private MethodDefinition ResolveMethodReference(MethodReference method)
+        {
+            var typeName = method.DeclaringType.FullName;
+            foreach (var asm in this.Assemblies)
+            {
+                foreach (ModuleDefinition module in asm.Modules)
+                {
+                    var type = module.Types[typeName];
+                    if (type == null)
+                        continue;
+
+                    return type.Methods.GetMethod(method.Name, method.Parameters);
+                }
+            }
+            return null;
+        }
+
+        public override void VisitMethodDefinition(MethodDefinition method)
+        {
+            
+            if (this.Assemblies.Count == 0) LoadAssemblies(method);
+            
+            if (this.Dependencies.Contains(method)) return;
+  
+            this.Dependencies.Add(method);
+
+            if (method.HasBody)
+            {
+                foreach (Instruction instruction in method.Body.Instructions)
+                {
+                    if (instruction.Operand != null)
+                    {
+                        switch (instruction.OpCode.Code)
+                        {
+                            case Code.Call:
+                            case Code.Callvirt:
+                            case Code.Newobj:
+                                break;
+                            default:
+                                continue;
+                        }
+                        var operand = instruction.Operand;
+                        
+                        if (operand is MethodReference)
+                        {
+
+                            var md = ResolveMethodReference(operand as MethodReference);
+                            if (md != null)
+                            {
+                                this.Graph.AddEdge(method.Name, md.Name);
+                                VisitMethodDefinition(md);
+                            }
+                        }
+                        else if (operand is MemberReference)
+                        {
+                            var mr = operand as MemberReference;
+                            this.Graph.AddEdge(method.Name, mr.Name);
+                            VisitMemberReference(mr);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        public override void VisitMemberReference(MemberReference member)
+        {
+            var methods = ResolveMemberReference(member);
+            foreach (var method in methods)
+                VisitMethodDefinition(method);
+        }
+    }
+}
