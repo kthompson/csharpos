15c6afb65b0b8ff58ea269e6da8c3452180acc46
Kevin Thompson <mrunleaded@gmail.com>
2010-03-01 20:35:21 -0800

    Added Option Parser to parse command line options.
    Signed-off-by: Kevin Thompson <mrunleaded@gmail.com>

---
+++ Compiler.Tests/OptionParserTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+
+namespace Compiler.Tests
+{
+    [TestFixture]
+    public class OptionParserTests
+    {
+        private class Options
+        {
+            public bool A { get; set; }
+            public bool B { get; set; }
+            public bool C { get; set; }
+            public string D { get; set; }
+        }
+
+        [Test]
+        [Row("/a", "/b")]
+        [Row("-a", "-b")]
+        [Row("--a", "--b")]
+        [Row("--alpha", "--bravo")]
+        public void Test1(string arg1, string arg2)
+        {
+            var options = new Options();
+            var unparsed = RunParser(options, new[]{arg1, arg2});
+            Assert.AreEqual(0, unparsed.Length);
+            Assert.IsTrue(options.A);
+            Assert.IsTrue(options.B);
+            Assert.IsFalse(options.C);
+            Assert.IsNull(options.D);
+        }
+
+        [Test]
+        [Row("/a", "/d=top", null)]
+        [Row("-a", "-d=top", null)]
+        [Row("-alpha", "-delta=top", null)]
+        [Row("--alpha", "--delta=top", null)]
+        [Row("/alpha", "/delta=top", null)]
+        [Row("/a", "/d", "top")]
+        [Row("-a", "-d", "top")]
+        [Row("-alpha", "-delta", "top")]
+        [Row("--alpha", "--delta", "top")]
+        [Row("/alpha", "/delta", "top")]
+        public void Test2(string arg1, string arg2, string arg3)
+        {
+            var args = arg3 == null ? new[] {arg1, arg2} : new[] {arg1, arg2, arg3};
+            var options = new Options();
+            var unparsed = RunParser(options, args);
+            Assert.AreEqual(0, unparsed.Length);
+            Assert.IsTrue(options.A);
+            Assert.IsFalse(options.B);
+            Assert.IsFalse(options.C);
+            Assert.AreEqual("top", options.D);
+        }
+
+        [Test]
+        public void GetUsageTests()
+        {
+            var options = new Options();
+            var parser = new OptionParser
+                             {
+                                 new Option {ShortForm = "a", LongForm = "alpha", Required= true, Action = () => options.A = true, Description = "Set the alpha standing"},
+                                 new Option {ShortForm = "b", LongForm = "bravo", Action = () => options.B = true, Description = "Set the bravo standing"},
+                                 new Option {ShortForm = "c", LongForm = "charlie", Action = () => options.C = true, Description = "Set the charlie standing"},
+                                 new Option {ShortForm = "d", LongForm = "delta", ActionWithParam = param => options.D = param, Description = "Set the delta standing"},
+                             };
+
+            Assert.Inconclusive(parser.GetUsage());
+
+        }
+
+        [Test]
+        [Row("/d")]
+        [Row("-d")]
+        [Row("--d")]
+        [Row("/delta")]
+        [Row("-delta")]
+        [Row("--delta")]
+        [ExpectedException(typeof(MissingOptionParameterException))]
+        public void MissingOptionParameterTest(string arg)
+        {
+            var options = new Options();
+            RunParser(options, "/a", arg);
+        }
+
+        [Test]
+        [Row("/b")]
+        [Row("-b")]
+        [Row("--b")]
+        [Row("/bravo")]
+        [Row("-bravo")]
+        [Row("--bravo")]
+        [ExpectedException(typeof(MissingOptionException))]
+        public void MissingOptionTest(string arg)
+        {
+            var options = new Options();
+            RunParser(options, arg);
+        }
+
+        private static string[] RunParser(Options options,params string[] args)
+        {
+            var parser = new OptionParser
+                             {
+                                 new Option {ShortForm = "a", LongForm = "alpha", Required= true, Action = () => options.A = true},
+                                 new Option {ShortForm = "b", LongForm = "bravo", Action = () => options.B = true},
+                                 new Option {ShortForm = "c", LongForm = "charlie", Action = () => options.C = true},
+                                 new Option {ShortForm = "d", LongForm = "delta", ActionWithParam = param => options.D = param},
+                             };
+
+            return parser.Parse(args);
+            
+        }
+    }
+}
---
+++ Compiler/OptionParser.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Compiler
+{
+    public abstract class OptionParserException : Exception
+    {
+        public Option Option { get; private set; }
+
+        protected OptionParserException(Option option, string message = "")
+            : base(message)
+        {
+            this.Option = option;
+        }
+    }
+
+    public class MissingOptionException : OptionParserException
+    {
+        public MissingOptionException(Option option, string message = "")
+            : base(option, message)
+        {
+
+        }
+    }
+
+    public class MissingOptionParameterException : OptionParserException
+    {
+        public MissingOptionParameterException(Option option, string message = "")
+            : base(option, message)
+        {
+            
+        }
+    }
+
+    public class Option
+    {
+        public string ShortForm { get; set; }
+        public string LongForm { get; set; }
+        public bool Required { get; set; }
+        public bool Found { get; set; }
+        public string Description { get; set; }
+
+        private Action _action;
+        public Action Action
+        {
+            get { return _action; }
+            set
+            {
+                if (_actionWithParam != null)
+                    throw new InvalidOperationException("You cannot set Action and ActionWithParam. Use only one.");
+                _action = value;
+            }
+        }
+
+        private Action<string> _actionWithParam;
+        public Action<string> ActionWithParam {
+            get { return _actionWithParam; }
+            set
+            {
+                if (_action != null)
+                    throw new InvalidOperationException("You cannot set Action and ActionWithParam. Use only one.");
+                _actionWithParam = value;
+            }
+        }
+
+        public bool RequiresParam
+        {
+            get
+            {
+                return this.ActionWithParam != null;
+            }
+        }
+    }
+
+    
+
+    public class OptionParser : IEnumerable<Option>
+    {
+        private readonly List<Option> _list = new List<Option>();
+
+        public string GetUsage()
+        {
+            using(var sw = new StringWriter())
+            {
+                foreach (var option in this)
+                {
+                    var sbRow = new StringBuilder();
+                    sbRow.Append('\t');
+                    var hasShort = !string.IsNullOrEmpty(option.ShortForm);
+                    var hasLong = !string.IsNullOrEmpty(option.LongForm);
+                    if (hasShort)
+                        sbRow.AppendFormat("-{0}", option.ShortForm);
+
+                    if (hasLong && hasShort)
+                        sbRow.Append(", ");
+
+                    if (hasLong)
+                    {
+                        if (option.RequiresParam)
+                            sbRow.AppendFormat("--{0} <{1}>", option.LongForm, option.LongForm.ToUpper());
+                        else
+                            sbRow.AppendFormat("--{0}", option.LongForm);
+                    }
+
+                    if(sbRow.Length >= 26)
+                    {
+                        //overflowed to the next line
+                        sw.WriteLine(sbRow.ToString());
+                        sw.Write("".PadRight(26));
+                    }
+                    else
+                    {
+                        sw.Write(sbRow.ToString().PadRight(26));
+                    }
+                    sw.WriteLine(option.Description);
+                }
+                return sw.ToString();
+            }
+        }
+
+        public string[] Parse(string[] args)
+        {
+            var stack = new Stack<string>(args.Reverse());
+            while (stack.Count > 0)
+            {
+                var option = stack.Pop();
+            
+                //the rest should be passed back.
+                if (option == "--")
+                    break;
+
+                //not an option so push it back onto the stack and give up
+                if ((option[0] != '/') && (option[0] != '-'))
+                {
+                    stack.Push(option);
+                    break;
+                }
+
+                //remove extra dash for long form
+                option = option.Substring(1);
+                if (option[0] == '-')
+                    option = option.Substring(1);
+
+                //if we have an equals sign, break it up
+                if (option.Contains('='))
+                {
+                    var values = option.Split(new[] {'='}, 2);
+                    option = values[0];
+                    stack.Push(values[1]);
+                }
+
+                var match = this.Where(o => o.LongForm == option || o.ShortForm == option).FirstOrDefault();
+                if (match == null)
+                {
+                    //cant execute the option so put it back.
+                    stack.Push(option);
+                    break;
+                }
+
+                //we are going to execute it
+                if (match.RequiresParam)
+                {
+                    //cant execute the option because we dont have a param supplied
+                    if (stack.Count == 0)
+                        throw new MissingOptionParameterException(match);
+
+                    match.ActionWithParam(stack.Pop());
+                }
+                else
+                {
+                    match.Action();
+                }
+
+                match.Found = true;
+            }
+
+            var notFound = this.Where(o => o.Required && !o.Found).FirstOrDefault();
+            if(notFound != null)
+                throw new MissingOptionException(notFound);
+
+            return stack.Reverse().ToArray();
+        }
+
+        public void Add(Option option)
+        {
+            this._list.Add(option);
+        }
+
+
+        #region IEnumerable<Option> Members
+
+        public IEnumerator<Option> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        #endregion
+    }
+}
