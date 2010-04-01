70682bb8de5e93c33ccd19fe2e1853cf63a0feea
Kevin Thompson <mrunleaded@gmail.com>
2010-03-31 23:28:45 -0700

    Incomplete
    Signed-off-by: Kevin Thompson <mrunleaded@gmail.com>

---
+++ Compiler/CilToX86Stage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Compiler.Framework;
+
+namespace Compiler
+{
+    public class CilToX86Stage : MethodCompilerStageBase
+    {
+        public override string Name
+        {
+            get { return "Cil to x86 Stage"; }
+        }
+
+        public override IMethodCompilerContext Run(IMethodCompilerContext context)
+        {
+            var code = context as CodeStream;
+            if (code == null)
+                return null;
+
+            foreach (var instruction in code)
+            {
+                
+            }
+
+            return base.Run(context);
+        }
+    }
+}
